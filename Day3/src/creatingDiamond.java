509a0845150ae8e522854fee51892cd31186ee6e
ylee862 <damy03121y@gmail.com>
2023-07-13 09:25:19 +0900

    creating diamond assignment
    couldn't finish it cause I got confused

---
+++ Day3/src/creatingDiamond.java
@@ -0,0 +1,21 @@
+public class creatingDiamond {
+
+    public static void main(String[] args) {
+
+        int size = 5;
+
+        for (int i=0; i <= size; i++) {
+            for (int j= size-i; j >= 1; j--) {
+                System.out.println(" ");
+            }
+
+            for (int k=0; k <= size; k++) {
+                
+            }
+
+        }
+    }
+
+
+
+}
--- Day3/src/movieAge.java
+++ Day3/src/movieAge.java
@@ -1,19 +1,19 @@
 public class movieAge {
     public static void main(String[] args) {
-        int age;
+        int age = 30;
 
         if (age < 0) {
             System.out.println("유아 관람불가");
         }
-        else if (0 <= age < 12) {
+        else if (0 <= age && age < 12) {
             System.out.println("전체 관람가");
         }
 
-        else if (12 <= age < 15) {
+        else if (12 <= age && age < 15) {
             System.out.println("12세 이상 관람가");
         }
 
-        else if (15 <= age < 19) {
+        else if (15 <= age && age < 19) {
             System.out.println("15세 이상 관람가");
         }
         else {
